e824592b9968dbe7d8d56b652b436163091562b0
guigacarvalho <guilherme_carvalho@homedepot.com>
2015-12-25 18:57:20 -0800

    Initial Commit

---
+++ virtualTourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  virtualTourist
+//
+//  Created by Guilherme Carvalho on 12/25/15.
+//  Copyright © 2015 Guilherme Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
