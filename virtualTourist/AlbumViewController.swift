4bd4376493131f0a2e1e5a67ac3c656dc66902ac
guigacarvalho <guilherme_carvalho@homedepot.com>
2016-01-02 06:40:00 -0800

    Setting up Collection View

--- virtualTourist/AlbumViewController.swift
+++ virtualTourist/AlbumViewController.swift
@@ -11,15 +11,26 @@ import MapKit
 import CoreLocation
 
 
-class AlbumViewController: UIViewController, MKMapViewDelegate {
-    
+class AlbumViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, MKMapViewDelegate {
+
     var latitude:Double?
     var longitude:Double?
     let regionRadius: CLLocationDistance = 10000
+    @IBOutlet weak var photoFlowLayout: UICollectionViewFlowLayout!
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let space: CGFloat = 0.0
+        let flowLength = (self.view.frame.size.width) / 3.0
+        
+        photoFlowLayout.minimumLineSpacing = space
+        photoFlowLayout.minimumInteritemSpacing = space
+        photoFlowLayout.itemSize = CGSizeMake(flowLength, flowLength)
+
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
         let location = CLLocation(latitude: self.latitude!, longitude: self.longitude!)
         let annotation = MKPointAnnotation()
@@ -28,8 +39,11 @@ class AlbumViewController: UIViewController, MKMapViewDelegate {
 
         centerMapOnLocation(location)
         
-        let methodArguments:[String: AnyObject] = FlickrAPI.sharedInstance().methodArguments(latitude!, lon: longitude!)
-        print(FlickrAPI.sharedInstance().getImageFromFlickrBySearch(methodArguments))
+//        let methodArguments:[String: AnyObject] = FlickrAPI.sharedInstance().methodArguments(latitude!, lon: longitude!)
+//        print(FlickrAPI.sharedInstance().getImageFromFlickrBySearch(methodArguments))
+        
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -41,6 +55,24 @@ class AlbumViewController: UIViewController, MKMapViewDelegate {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
             regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("photoItem", forIndexPath: indexPath) as! PhotoCell
+        cell.photoImageView.image = UIImage(named: "placeholder")
+        return cell
+
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath:NSIndexPath)
+    {
+        
+        print(indexPath.row)
+//        self.navigationController!.pushViewController(detailController, animated: true)
+        
     }
 
 }
---
+++ virtualTourist/PhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCell.swift
+//  virtualTourist
+//
+//  Created by Guilherme Carvalho on 1/2/16.
+//  Copyright © 2016 Guilherme Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+}
